b8738afdace29acb06b32c134b6893cf77e9c8c1
DouglasAnjos7 <44895883+DouglasAnjos7@users.noreply.github.com>
2021-07-15 23:24:15 -0300

    One more
    Homework: https://github.com/svdmaar/homework/blob/master/saturday_crew_210627.md

---
+++ douglas_210715_01.py
@@ -0,0 +1,16 @@
+for numbers in range(100):                                              #Question2
+    if numbers < 10:
+        print(numbers)
+    elif numbers > 10 and numbers < 20:
+        print(numbers)
+    elif numbers > 20 and numbers < 30:
+        print(numbers)
+    elif numbers > 30 and numbers < 40:
+        print(numbers)
+    elif numbers > 40:
+        print(numbers)
+    else:
+        print("Hmm I don't think so...")
+
+dictionary = {"fruit": "banana", "number": 5, "list": ["Vw", "Ferrari", "Ford"]}
+print(dictionary)
